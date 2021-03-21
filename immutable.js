868ee1f5abd9aa5688f2b74f5def51b475471699
charliejuc <charliejuc@gmail.com>
2021-03-21 11:11:59 +0100

    1. Mutability / Immutability

---
+++ immutable.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const arr = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+const randomComparator = (a, b) => Math.random() > .5 ? a - b : b - a
+const orderedArr = arr.slice().sort(randomComparator)
+
+console.log({firstValueAfterOrder: orderedArr[0]})
+console.log({firstValueBeforeOrder: arr[0]})
+
+console.log(orderedArr)
+
+console.log('orderedArr === arr:', orderedArr === arr)
+console.log('arr === arr.slice():', arr === arr.slice())

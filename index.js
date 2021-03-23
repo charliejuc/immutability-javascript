155c31d1b0e744c68971dd3b0b1364dff7dc7bcb
charliejuc <charliejuc@gmail.com>
2021-03-23 18:41:44 +0100

    Undo / Redo

--- index.js
+++ index.js
@@ -1,26 +1,79 @@
 'use strict'
 
-// Structural Sharing
-
-const user = {
-    id: 1,
-    username: 'Mike',
-    address: {
-        street: 'A street',
-        number: 14
+let states = {
+    undo: [],
+    redo: []
+}
+let state = {
+    logged: true,
+    premium: false,
+    user: {
+        id: 1,
+        username: 'Anna'
     },
-    profile: {
-        nickname: 'Mikey',
-        image: 'bff7a93.jpg'
+    todos: []
+}
+
+const prependState = (kind, states, state) => ({
+    ...states,
+    [kind]: [state, ...states[kind]]
+})
+const appendTodo = (state, todo) => ({
+    ...state,
+    todos: [...state.todos, todo]
+})
+
+states = prependState('undo', states, state)
+state = appendTodo(state, {
+    id: 1,
+    message: 'Make the bed'
+})
+
+console.log('Save Todo:', state.todos)
+
+states = prependState('undo', states, state)
+state = appendTodo(state, {
+    id: 2,
+    message: 'Record the course'
+})
+
+console.log('Save Second Todo:', state.todos)
+
+const stateUndo = (states, index) => ({
+    state: states.undo[index],
+    states: {
+        ...states,
+        undo: [...states.undo.slice(index + 1)]
     }
-}
+})
+const stateRedo = (states, index) => ({
+    state: states.redo[index],
+    states: {
+        ...states,
+        redo: [...states.redo.slice(index + 1)]
+    }
+})
 
-const updatedUser = {
-    ...user,
-    username: 'mike1234'
-}
+console.log()
+console.log('UNDO')
+states = prependState('redo', states, state)
+;({state, states} = stateUndo(states, 0))
 
-console.log('user === updatedUser', user === updatedUser)
-// address and profile have not been duplicated
-console.log('user.address === updatedUser.address', user.address === updatedUser.address)
-console.log('user.profile === updatedUser.profile', user.profile === updatedUser.profile)
+console.log('First Undo', state.todos)
+
+states = prependState('redo', states, state)
+;({state, states} = stateUndo(states, 0))
+
+console.log('Second Undo', state.todos)
+
+console.log()
+console.log('REDO')
+states = prependState('undo', states, state)
+;({state, states} = stateRedo(states, 0));
+
+console.log('First Redo', state.todos)
+
+states = prependState('undo', states, state)
+;({state, states} = stateRedo(states, 0))
+
+console.log('Second Redo', state.todos)
